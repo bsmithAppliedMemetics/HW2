e3a596b4fd476f0a55d4432f3dc24ca3158dbb79
bsmithAppliedMemetics <b.smith@AppliedMemeticsllc.com>
2017-09-21 09:19:25 -0400

    lowercase Fix

--- HW2.js
+++ HW2.js
@@ -1,9 +1,8 @@
 var dogs = ['Pug', 'Golden Retreiver', 'Black Lab', 'Dachshund', 'Beagle'];
-var fav = 'Pug';
 var input = prompt('What is your favorite dog breed?');
 var randomDog = dogs[Math.floor(Math.random()*5)];
-console.log('Your favorite breed is ' + fav + '? Mine is ' + randomDog);
-if(randomDog.toLowerCase == input.toLowerCase)
+console.log('Your favorite breed is ' + input + '? Mine is ' + randomDog + '!');
+if(randomDog.toLowerCase() == input.toLowerCase())
 {
 	console.log("We're besties!");
 }
